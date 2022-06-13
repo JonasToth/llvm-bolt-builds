cb5935c547f7e8dcc7aeb341d72e8d843aefb5eb
Peter Jung <admin@ptr1337.dev>
2022-06-13 22:00:15 +0200

    formatting

--- bolt-anything.bash
+++ bolt-anything.bash
@@ -20,60 +20,60 @@ mkdir -p ${FDATA}
 mkdir -p ${BOLTBIN}
 
 check_requirements() {
-readelf -p .rela.text ${BINARYPATH}/${BINARY}
+    readelf -p .rela.text ${BINARYPATH}/${BINARY}
 }
 
 check_reloc=$(readelf -p .rela.text ${BINARYPATH}/${BINARY} | grep ".rela.text")
 
-  instrument() {
-      echo "Instrument binary with llvm-bolt"
+instrument() {
+    echo "Instrument binary with llvm-bolt"
 
-      ${BOLTPATH}/llvm-bolt \
-          --instrument \
-          --instrumentation-file-append-pid \
-          --instrumentation-file=${FDATA}/${BINARY}.fdata \
-          ${BINARYPATH}/${BINARY} \
-          -o ${BOLTBIN}/${BINARY}
+    ${BOLTPATH}/llvm-bolt \
+        --instrument \
+        --instrumentation-file-append-pid \
+        --instrumentation-file=${FDATA}/${BINARY}.fdata \
+        ${BINARYPATH}/${BINARY} \
+        -o ${BOLTBIN}/${BINARY}
 
-      sudo mv ${BINARYPATH}/${BINARY} ${BINARYPATH}/${BINARY}.org
-      sudo cp ${BOLTBIN}/${BINARY} ${BINARYPATH}/${BINARY}
-  }
+    sudo mv ${BINARYPATH}/${BINARY} ${BINARYPATH}/${BINARY}.org
+    sudo cp ${BOLTBIN}/${BINARY} ${BINARYPATH}/${BINARY}
+}
 
-  optimize() {
-      echo "Merging generated profiles"
-      ${BOLTPATH}/merge-fdata ${FDATA}/${BINARY}*.fdata > ${BOLTBIN}/${BINARY}-combined.fdata
+optimize() {
+    echo "Merging generated profiles"
+    ${BOLTPATH}/merge-fdata ${FDATA}/${BINARY}*.fdata > ${BOLTBIN}/${BINARY}-combined.fdata
 
-      echo "Optimizing binary with generated profile"
-      ${BOLTPATH}/llvm-bolt ${BINARYPATH}/${BINARY}.org \
-          --data ${BOLTBIN}/${BINARY}-combined.fdata \
-          -o ${BOLTBIN}/${BINARY}.bolt \
-          -relocs \
-          -split-functions=3 \
-          -split-all-cold \
-          -icf=1 \
-          -lite=1 \
-          -split-eh \
-          -use-gnu-stack \
-          -jump-tables=move \
-          -dyno-stats \
-          -reorder-functions=hfsort \
-          -reorder-blocks=ext-tsp \
-          -tail-duplication=cache || (echo "Could not optimize the binary"; exit 1)
+    echo "Optimizing binary with generated profile"
+    ${BOLTPATH}/llvm-bolt ${BINARYPATH}/${BINARY}.org \
+        --data ${BOLTBIN}/${BINARY}-combined.fdata \
+        -o ${BOLTBIN}/${BINARY}.bolt \
+        -relocs \
+        -split-functions=3 \
+        -split-all-cold \
+        -icf=1 \
+        -lite=1 \
+        -split-eh \
+        -use-gnu-stack \
+        -jump-tables=move \
+        -dyno-stats \
+        -reorder-functions=hfsort \
+        -reorder-blocks=ext-tsp \
+        -tail-duplication=cache || (echo "Could not optimize the binary"; exit 1)
 
-      echo "You can find now your optimzed binary at ${BOLTBIN}"
-      sudo rm -rf ${FDATA}/${BINARY}.fdata*
-      sudo cp ${BOLTBIN}/${BINARY}.bolt ${BINARYPATH}/${BINARY}
-  }
+    echo "You can find now your optimzed binary at ${BOLTBIN}"
+    sudo rm -rf ${FDATA}/${BINARY}.fdata*
+    sudo cp ${BOLTBIN}/${BINARY}.bolt ${BINARYPATH}/${BINARY}
+}
 
 
 
 if [ "$(echo "${check_requirements}" | grep -i rela.text)" = "" ]; then
 
-if [ ${STAGE} = 1 ]; then
-instrument
-fi
-if [ ${STAGE} = 2 ]; then
-optimize
-fi
+    if [ ${STAGE} = 1 ]; then
+        instrument
+    fi
+    if [ ${STAGE} = 2 ]; then
+        optimize
+    fi
 
 fi
